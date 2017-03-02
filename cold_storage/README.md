b23954fce7cb253b0d158e9a48ae4aa648eea9eb
metalab <jessica@metalab.harvard.edu>
2017-03-02 16:42:35 -0500

    add stuff

---
+++ cold_storage/README.md
@@ -0,0 +1,18 @@
+
+# cold_storage
+
+The web documentary for the [Cold Storage](librarybeyondthebook.org/cold_storage) project
+
+## [Start Bootstrap](http://startbootstrap.com/) - [Grayscale](http://startbootstrap.com/template-overviews/grayscale/)
+
+[Grayscale](http://startbootstrap.com/template-overviews/grayscale/) is a multipurpose, one page HTML theme for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/). This template features various content sections and a Google Maps section with a custom map marker.
+
+### Creator
+
+Start Bootstrap was created by and is maintained by **David Miller**, Managing Parter at [Iron Summit Media Strategies](http://www.ironsummitmedia.com/).
+
+Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework created by [Mark Otto](https://twitter.com/mdo) and [Jacob Thorton](https://twitter.com/fat).
+
+### Copyright and License
+
+Copyright 2013-2015 Iron Summit Media Strategies, LLC. Code released under the [Apache 2.0](https://github.com/IronSummitMedia/startbootstrap-grayscale/blob/gh-pages/LICENSE) license.
---
+++ cold_storage/js/vidplayer.js
@@ -0,0 +1,211 @@
+  //Loads Youtube IFrame Player API:
+  var tag = document.createElement('script');
+  
+  // intro video vars
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var intro;
+
+  // main video vars
+  var secondtag = document.createElement('script');
+  secondtag.src = "https://www.youtube.com/iframe_api";
+  var secondScriptTag = document.getElementsByTagName('script')[0];
+  secondScriptTag.parentNode.insertBefore(secondtag, secondScriptTag);
+      var module;    
+  var currentModule = 0;
+  var main_player;
+
+  // floorplan nav
+  var jumplinks = [];
+  
+  // create YT video objects
+  function onYouTubeIframeAPIReady() {
+      intro = new YT.Player('intro', {
+          height: 563,
+          width: 1000,
+          videoId: 'IYFuz8d8WzQ',
+          playerVars: {
+                   'autoplay':1,
+                   'controls':0,
+                   'showinfo':0,
+                   'modestbranding':1,
+                   'rel':0},
+          events: {
+              'onReady': onPlayerReady,
+              'onStateChange': onIntroPlayerStateChange
+              }
+          });
+
+      main_player = new YT.Player('main_player', {
+          height: '563',
+          width: '1000',
+          videoId: 'quIRtrq9K-E',
+          playerVars: {
+                       'autoplay':0,
+                       'controls':0,
+                       'showinfo':0,
+                       'modestbranding':1,
+                       'rel':0},
+          events: {
+                  // 'onReady': onPlayerReady,
+                  'onStateChange': onMainPlayerStateChange
+                }
+      });
+     
+      // initiate floorplan navigation
+      // Add event listeners to each fp section
+
+      for(var i = 1; i <= 7; i++){
+        var jumplink = $('#fp'+(i));
+        var t =jumplink.data("start");
+        jumplink.click(floorNav(i, t));
+      }
+
+      // "enter" button event listener pauses intro, which in turn plays main
+      $("#enter").click(function(){ intro.pauseVideo();});
+
+      // "vault_null" click event pauses intro, plays main vid
+      $("#vault_null").click(function(){ intro.pauseVideo(); });
+
+      // waypoints: pause videos when no longer in focus
+      var introWaypt = new Waypoint({
+        element: $("#intro"),
+        handler: function(direction){
+
+        }
+      });
+
+      var mainWaypt = new Waypoint({
+        element: $("#main_player"),
+        handler: function(direction){
+          intro.pauseVideo(); // triggers main player & vault also
+          console.log("main waypt "+direction);
+          if(direction=="up"){
+            main_player.playVideo();
+          }
+        }
+      });
+
+      var floorplanWaypt = new Waypoint({
+        element: $("#floorplan"),
+        handler: function(){
+          console.log("floorplan waypt");
+          main_player.pauseVideo();
+        }
+      })
+
+  }                                 
+
+  //Initiates playback
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+              
+ //Trigger movement to next ID on playback stop - making this up
+  function onIntroPlayerStateChange(event){
+        if (event.data === 2){
+        
+            $("html, body").animate({scrollTop : $('#main_player').offset().top }, 600);
+            main_player.playVideo();
+            } 
+         }
+                      
+  //Trigger pause in video if user moves to the next ID
+
+
+function onMainPlayerStateChange(event) {
+    var t;
+    if (event.data == YT.PlayerState.PLAYING) {
+      setInterval(function(){
+        t = main_player.getCurrentTime();
+
+        if(t < 224) { module = 1;   modulename = "one"; }
+        else if(t< 353) { module = 2; modulename = "two"; }
+        else if(t< 613) { module = 3; modulename = "three"; }
+        else if(t< 726) { module = 4; modulename = "four"; }
+        else if(t< 1193) { module = 5; modulename = "five"; }
+        else if(t< 1373) { module = 6; modulename = "six"; }
+        else {
+          module = 7;
+          modulename = "seven";
+        }
+
+        var count=0;
+        var vaultdata = {};
+
+        $.getJSON('data/coldstorage.json', function(chone) {
+          count=chone.length;
+          var firstIndex=0,lastIndex=0;
+          $.each(chone, function(i,val) {
+            if(!vaultdata[val.chapter]) vaultdata[val.chapter] = [];
+            vaultdata[val.chapter].push(val);
+          });
+
+          // run after JSON has loaded
+          if(module != currentModule) {
+            $("#fp"+currentModule).attr("src", "./img/FloorplanNav/"+currentModule+"_off.jpg");
+            currentModule = module;
+            console.log("MODULE CHANGE");
+            console.log("module "+currentModule);
+            console.log(modulename);
+            $("#fp"+currentModule).attr("src", "./img/FloorplanNav/"+currentModule+"_on.jpg");
+            //$(".chapter").hide();
+            $("."+modulename).show();
+            $("#currentembed").html("");
+            var v = vaultdata[module][0]; 
+            $("#currentembed").append($("<a></a>") .attr({"class":"tracks","itemid":v.itemid+'_tracks',"href":v.itemid,"data-file":v.trackFile}).html(v.location));
+            console.log(v);
+            $('#player-digital-title-one').html(v.description);
+            $('#player-digital-name-one').html(v.item_title);
+          }
+          $("#vault_null").hide();
+          $("#infobox").show();
+
+        });
+
+      },1000);
+
+  var clickIndex = 0;
+  $("#next-bt").click(function() {
+      clickIndex = clickIndex + 1;;       
+      if(clickIndex >= vaultdata[currentModule].length) clickIndex = 0;
+    $("#currentembed").html("");
+    var v = vaultdata[currentModule][clickIndex];
+    $("#currentembed").append($("<a></a>") .attr({"class":"tracks","itemid":v.itemid+'_tracks',"href":v.itemid,"data-file":v.trackFile}).html(v.location));
+      $('#player-digital-title-one').html(v.description);
+      $('#player-digital-name-one').html(v.item_title);
+      var vaultshow=document.getElementById("vault_null");
+      vaultshow.style.display = "none";
+      var infoshow=document.getElementById("infobox");
+      infoshow.style.display = "block";
+  });
+
+  $("#prev-bt").click(function() {
+      clickIndex = clickIndex - 1;;       
+      if(clickIndex <= 0) clickIndex = vaultdata[currentModule].length - 1;
+      $("#currentembed").html("");
+      var v = vaultdata[currentModule][clickIndex];
+      $("#currentembed").append($("<a></a>") .attr({"class":"tracks","itemid":v.itemid+'_tracks',"href":v.itemid,"data-file":v.trackFile}).html(v.location));
+      $('#player-digital-title-one').html(v.description);
+      $('#player-digital-name-one').html(v.item_title);
+      var vaultshow=document.getElementById("vault_null");
+      vaultshow.style.display = "none";
+      var infoshow=document.getElementById("infobox");
+      infoshow.style.display = "block";
+  });
+}
+}
+  function stopVideo() {
+    main_player.stopVideo();
+  }
+
+// Navigation from the floor plan
+function floorNav(j, t){
+  return function (e){
+    main_player.seekTo(t);
+    main_player.pauseVideo();
+  }
+
+}
+
